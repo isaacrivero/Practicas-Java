9e76b93bcc8332289ead936331386e8877dd42dc
pepesan <pepesan@gmailcom>
2019-01-29 15:34:56 +0100

    incluido Java 8 a nivel de interfaces

--- src/main/java/com/ejemplos/clases/interfaces/Interfaz.java
+++ src/main/java/com/ejemplos/clases/interfaces/Interfaz.java
@@ -3,4 +3,16 @@ package com.ejemplos.clases.interfaces;
 public interface Interfaz {
 	void funcion1();
 	int function2(Integer i);
+
+	//nueva incorporación implementación por defecto en Java 8
+    default void log(String str){
+        System.out.println("I1 logging::"+str);
+    }
+
+    // Método stático Java 8
+    static boolean isNull(String str) {
+        System.out.println("Interface Null Check");
+
+        return str == null ? true : "".equals(str) ? true : false;
+    }
 }
